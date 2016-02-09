0f8efbd0f1c15f44c4bff3b01cd0dc7b4cbf2a06
Dan Gonzalez <d.gonzalez0902@gmail.com>
2016-02-09 15:28:20 -0700

    Added a tic tac toe project

---
+++ TicTacToeTest/TicTacToeTest/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  TicTacToeTest
+//
+//  Created by Dan Gonzalez on 2016-02-09.
+//  Copyright © 2016 varyag00. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //MARK: Attributes  
+    
+    @IBOutlet weak var Board: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
