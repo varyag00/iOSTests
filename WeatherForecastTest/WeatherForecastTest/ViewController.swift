dc8a137a1c9f6aa4de31d8e896d2907dd13f42de
Dan Gonzalez <d.gonzalez0902@gmail.com>
2016-02-08 21:56:21 -0700

    Completed weatherviewer app

--- WeatherForecastTest/WeatherForecastTest/ViewController.swift
+++ WeatherForecastTest/WeatherForecastTest/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Attributes
+    
+    @IBOutlet weak var CityTextField: UITextField!
+    @IBOutlet weak var WebView: UIWebView!
+    
+    var city:String = ""
+    var url = NSURL(string: "http://www.weather-forecast.com/locations/Calgary/forecasts/latest")!
+    let urlString = NSString(string: "http://www.weather-forecast.com/locations/<city>/forecasts/latest")
+
+    
+    //MARK: Actions
+    
+    @IBAction func DoneButtonClicked(sender: AnyObject) {
+        
+        if CityTextField.text?.characters.count > 0 {
+            city = String(UTF8String: CityTextField.text!)!
+            
+            //newUrlString should now contain a valid URL if valid city given
+            let newUrlString = urlString.stringByReplacingOccurrencesOfString("<city>", withString: city)
+            url = NSURL(string: newUrlString)!
+            
+            WebView.loadRequest(NSURLRequest(URL: url))
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +45,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    //CODE YOU ADD ANY TIME YOU USE A KEYBOARD
+    
+    //when user clicks outside of keyboard, end editing (close keyboard)
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    //close keyboard when return pressed
+    func textFieldShouldReturn(textField: UITextField!) -> Bool{
+        
+        //could also probably trigger button functionality
+        
+        textField.resignFirstResponder()    //close the keyboard
+        return true
+    }
 
 }
 
